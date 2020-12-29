9f5339d840e2c5c41fd3f25270ec80ca1cb8d498
Matthew Wicker <mdwicker@gmail.com>
2020-12-29 09:49:08 -0800

    Implement grid size settings

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 // start out the game with a 16x16 grid
-let initialGridSize = 16;
-initializeGrid(initialGridSize);
+let gridSize = 16;
+initializeGrid(gridSize);
 
+// listen for the requested grid size and update live
+const sizeNumber = document.getElementById("size-number");
+const sizeSlider = document.getElementById("size-slider")
+
+sizeNumber.addEventListener("input", function () {
+    gridSize = this.value;
+    initializeGrid(gridSize);
+});
+
+sizeSlider.addEventListener("input", function () {
+    gridSize = this.value;
+    initializeGrid(gridSize);
+});
+
+
+// when the "New Board" button is pressed, generate a new board of the requested size
+const newBoardButton = document.getElementById("new-board");
+newBoardButton.addEventListener("click", function () {
+    initializeGrid(gridSize);
+});
 
 
 // set up a new square grid with the given number of boxes per side
 function initializeGrid(size) {
     const container = document.getElementById("grid-container");
+    container.innerHTML = "";
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
